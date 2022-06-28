b71703d9857d41bf9ad4e4dde495445fb5b4b15a
Edisson Castillo <andreskmpa@gmail.com>
2022-06-27 22:12:51 -0500

    deploying portafolio

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {Routes, Route } from "react-router-dom";
+import GlobalStyle from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import {lightTheme} from "./components/Themes";
+import Main from "./components/Main";
+import Blogpage from "./components/BlogPage";
+import AboutPage from "./components/AboutPage";
+import MySkillsPage from "./components/MySkillsPage";
+import WorkPage from "./components/WorkPage";
+
+
+
+function App() {
+  return (
+    <>
+    <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
+
+      <Routes>
+        <Route  path="/" element={<Main />} />
+        <Route  path="/about" element={<AboutPage />} />
+        <Route  path="/blog" element={<Blogpage />} />
+        <Route  path="/work" element={<WorkPage />} />
+        <Route  path="/skills" element={<MySkillsPage />} />
+      </Routes>
+
+    </ThemeProvider> 
+    </>
+  );
+}
+
+export default App;
+
